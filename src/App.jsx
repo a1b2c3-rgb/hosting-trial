6c5926be189a4414be9cbbbd091fbbdb5d3b1881
violet <violettalemwa2@gmail.com>
2026-03-09 14:37:58 +0300

    second cmommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Homepage from './pages/homepage'
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
